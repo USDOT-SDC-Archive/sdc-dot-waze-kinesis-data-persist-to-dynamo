24cd2ff1016d1b55fc2d1760d18ae0c1693d04c2
swarnadee bapat <swarnadee.bapat@reancloud.com>
2018-07-20 22:16:06 +0530

    Persist data to dynamodb

---
+++ common/utils.py
@@ -0,0 +1,59 @@
+from common.logger_utility import *
+from common.constants import *
+import csv
+import gzip
+import uuid
+import boto3
+import datetime
+import os
+from boto3.dynamodb.conditions import Attr
+import re
+import time
+
+
+class Utils:
+    pass
+
+    @staticmethod
+    def persist_record_to_dynamodb_table(s3_key, table_name, state, num_records, bucket, batch_id, is_historical, month, year):
+        try:
+            dynamodb_curated_records_table_name = os.environ['DDB_CURATED_RECORDS_TABLE_ARN'].split('/')[1]
+            persist_records = bool(int(os.environ['PERSIST_RECORDS']))
+            s3_key = "s3://" + bucket + "/" + s3_key
+
+            if persist_records:
+                dynamodb = boto3.resource('dynamodb')
+                curated_record_table = dynamodb.Table(dynamodb_curated_records_table_name)
+                response = curated_record_table.put_item(
+                    Item={
+                        'CurationRecordId': str(uuid.uuid4()),
+                        'BatchId': batch_id,
+                        'DataTableName': table_name,
+                        'S3Key': s3_key,
+                        'State': state,
+                        'TotalNumCuratedRecords': num_records,
+                        'IsHistorical': is_historical,
+                        'Year': year,
+                        'Month': month
+                    }
+                )
+                LoggerUtility.logInfo("Successfully persisted record to dynamo db table - {}".format(response))
+            else:
+                LoggerUtility.logInfo("Persist records flag is disabled, so not persisting "
+                                      "any records to dynamodb table")
+
+        except Exception as e:
+            LoggerUtility.logError("Failed to persist record to dynamo db table for key - {}".format(s3_key))
+            raise e
+
+    @staticmethod
+    def get_parsed_string(data):
+        try:
+            if data != "" and data != "null" and data != "NULL" and data is not None:
+                data = data.replace(r"'", r"\'")
+                data = data.replace("\n","")
+                data = data.replace("\r","")
+
+        except Exception as e:
+            raise e
+        return data
---
+++ waze_persist_to_dynamo_handler_main.py
@@ -0,0 +1,55 @@
+import os
+import json
+import traceback
+import base64
+import boto3
+from common.logger_utility import *
+from common.utils import *
+
+
+def lambda_handler(event, context):
+    LoggerUtility.setLevel()
+    LoggerUtility.logInfo("Event data from previous function is - {}".format(event))
+    try:
+        # Fetch records from event and decode records
+        __persist_to_dynamodb(event["Records"])
+        
+    except Exception as data_handling_error:
+        LoggerUtility.logError("FATAL: Failed to process record- " + str(data_handling_error))
+        LoggerUtility.logError("FATAL: Stacktrace- " + str(traceback.format_exc()))
+
+def __decode_data(record):
+    LoggerUtility.logInfo("KINESIS SEQUENCE NUMBER: " + str(record["kinesis"]["sequenceNumber"]) + " || KINESIS EVENT ID: " + record["eventID"])
+    encoded_string = record["kinesis"]["data"]
+    return json.loads(base64.b64decode(encoded_string).decode())
+
+def __persist_to_dynamodb(records):
+    """Decode the base64 data."""
+    for index,record in enumerate(records):
+        decoded_json = __decode_data(record)
+        state = decoded_json.get("state")
+        month = decoded_json.get("month")
+        num_records = decoded_json.get("num-records")
+        is_historical = decoded_json.get("is-historical")
+        table_name = decoded_json.get("table-name")
+        year = decoded_json.get("year")
+        bucket = decoded_json.get("bucket-name")
+        s3_key = decoded_json.get("s3-key")
+
+        batch_id = __get_latest_batch()
+
+        Utils.persist_record_to_dynamodb_table(s3_key, table_name, state, num_records, bucket, batch_id, is_historical, month, year)
+        
+    LoggerUtility.logInfo("Data persisted to dynamodb successfully from Kinesis!")
+
+def __get_latest_batch():
+    try:
+        ssm = boto3.client('ssm')
+        response = ssm.get_parameter(Name='LatestBatchId', WithDecryption=False)
+        LoggerUtility.logInfo("Response from parameter store - {}".format(response))
+        current_batch_id = response["Parameter"]["Value"]
+    except Exception as ex:
+        LoggerUtility.logError("Unable to get latest batch with reason - {}".format(ex))
+        raise ex
+    return current_batch_id
+
