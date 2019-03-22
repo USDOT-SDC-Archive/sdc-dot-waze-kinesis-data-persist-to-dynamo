2bb81f09314c9694662fd03afd57769d74315844
Santosh Karla <santosh.karla@gmail.com>
2019-03-22 11:53:54 -0400

    Pull variable name from environment variables

--- waze_persist_to_dynamo_handler_main.py
+++ waze_persist_to_dynamo_handler_main.py
@@ -5,6 +5,7 @@ import base64
 import boto3
 from common.logger_utility import *
 from common.utils import *
+from lambdas.waze_persist_to_dynamo_lambda_handler import *
 
 
 def lambda_handler(event, context):
@@ -24,6 +25,7 @@ def __decode_data(record):
     return json.loads(base64.b64decode(encoded_string).decode())
 
 def __persist_to_dynamodb(records):
+    LoggerUtility.logInfo("Records count {} ".format(len(records)))
     """Decode the base64 data."""
     for index,record in enumerate(records):
         decoded_json = __decode_data(record)
@@ -38,6 +40,8 @@ def __persist_to_dynamodb(records):
         batch_id = __get_latest_batch()
         LoggerUtility.logInfo("s3_key:"+s3_key)
         data_set = s3_key.split("/")[0]
+        LoggerUtility.logInfo("data_set:"+data_set)
+        LoggerUtility.logInfo("BATCH_ID:"+batch_id)
         if data_set == "waze":
             Utils.persist_record_to_dynamodb_table(s3_key, table_name, state, num_records, bucket, batch_id, is_historical, month, year)
         else:
@@ -47,11 +51,11 @@ def __persist_to_dynamodb(records):
 def __get_latest_batch():
     try:
         ssm = boto3.client('ssm')
-        response = ssm.get_parameter(Name='LatestBatchId', WithDecryption=False)
+        latest_batch_id = os.environ["LATEST_BATCH_ID"]
+        response = ssm.get_parameter(Name=latest_batch_id, WithDecryption=False)
         LoggerUtility.logInfo("Response from parameter store - {}".format(response))
         current_batch_id = response["Parameter"]["Value"]
     except Exception as ex:
         LoggerUtility.logError("Unable to get latest batch with reason - {}".format(ex))
         raise ex
     return current_batch_id
-
